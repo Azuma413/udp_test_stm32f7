ee998b96a4f4683003cb6cbb2f007ee111140ced
Azuma413 <hirekatsu0523@gmail.com>
2023-02-24 19:50:34 +0900

    printf対応など

--- Core/Src/rtos_udp.c
+++ Core/Src/rtos_udp.c
@@ -22,13 +22,13 @@ void UDPDefineTasks() {
 
 void UDPSendReceive(void const *argument) {
 	printf("ender udp task\r\n");
-	fd_set rset;
-	int sock;
+	fd_set reading;
+	int sock1;
 	int iBytesWritten = 0;
 	char rxbuf[512]; //最大受信データサイズ
 	char txbuf[256]; //最大送信データサイズ
 	struct sockaddr_in myAddr, cAddr;
-	sock = lwip_socket(AF_INET, SOCK_DGRAM, 0); //udp制御ブロックを作成
+	sock1 = lwip_socket(AF_INET, SOCK_DGRAM, 0); //udp制御ブロックを作成
 
 	//ソケットを作成
 	memset((char*) &myAddr, 0, sizeof(myAddr)); //myAddrを0で埋める
@@ -42,19 +42,14 @@ void UDPSendReceive(void const *argument) {
 	cAddr.sin_addr.s_addr = inet_addr(PC_ADDR); //PCのIP
 	cAddr.sin_port = htons(PC_PORT); //PCのポート
 
-	int err = lwip_bind(sock, (struct sockaddr*) &myAddr, sizeof(myAddr)); //制御ブロックをIPとポートに紐づける。
+	int err = lwip_bind(sock1, (struct sockaddr*) &myAddr, sizeof(myAddr)); //制御ブロックをIPとポートに紐づける。
 	if (err != 0) {
 		printf("UDPController:ERROR \r\n");
 	} else {
 		printf("UDPController:Socket Opened!\r\n");
 	}
-
-	FD_ZERO(&rset); //ディスクリプタ集合の初期化
-
-	int timeout = 0;
+	int maxfd = 0;
 	int result = 0;
-	struct receive_data data_null; //受信を失敗したとき用のデータ
-	bzero(&data_null, sizeof(struct receive_data)); //初期化
 	/*---------test sumple----------*/
 	f7_data.omni_x = 0.5f;
 	f7_data.omni_y = 0.5f;
@@ -66,39 +61,29 @@ void UDPSendReceive(void const *argument) {
 
 	printf("enter udp loop\r\n");
 	while (1) {
-		FD_SET(sock, &rset); //rsetにsock（ディスクリプタ番号）を追加
+		FD_ZERO(&reading); //ディスクリプタ集合の初期化
+		FD_SET(sock1, &reading); //readingにsock（ディスクリプタ番号）を追加
+		maxfd = sock1;
+		maxfd ++;
+		memset(&tv, 0, sizeof(tv));
 		//tv.tv_usec = 1000;
 		tv.tv_sec = 5;
-		result = select(sock + 1, &rset, NULL, NULL, &tv); //ファイルディスクリプタ―がready状態になるまで1ミリ秒まで待つ。
+		result = select(maxfd, &reading, NULL, NULL, &tv); //ファイルディスクリプタ―がready状態になるまで1ミリ秒まで待つ。
 		printf("select result: %d\r\n", result);
-		if (timeout < 100) {
-			timeout++;
-		} else {
-			printf("timeout\r\n");
-			memcpy(&ros_data, &data_null, sizeof(struct receive_data)); //100回ループしたら0データを渡す。
-			timeout = 0;
+		if (result == -1){
+			//error
+			continue;
 		}
-		if (FD_ISSET(sock, &rset)) { //rsetの中にsockの値が含まれているか調べる。
+		if (result == 0){
+			//timeout
+			continue;
+		}
+		if (FD_ISSET(sock1, &reading)) { //readingの中にsockの値が含まれているか調べる。
 			printf("fd is set\r\n");
 			socklen_t n;
 			socklen_t len = sizeof(cAddr);
-			struct send_data* sd = (struct send_data*)&txbuf; //txbufの位置に重ねてsdを宣言
-			memcpy(sd, &f7_data, sizeof(struct send_data)); //送信データをコピーする
-			iBytesWritten = lwip_sendto(sock, (char*)txbuf, sizeof(txbuf), 0, (struct sockaddr*)&cAddr, sizeof(cAddr));
-			//iBytesWritten = lwip_sendto(sock, (char*)"hello world from f7!", 256, 0, (struct sockaddr*)&cAddr, sizeof(cAddr));
-			if (iBytesWritten > 0) {
-				printf("success send data\r\n");
-				if (iBytesWritten < sizeof(struct send_data)) {
-					printf("send data invalid\r\n");
-				}else{
-					printf("send data is perfect\r\n");
-				}
-			}else{
-				printf("failed send data\r\n");
-			}
-			n = lwip_recvfrom(sock, (char*) rxbuf, sizeof(rxbuf), (int) NULL, (struct sockaddr*) &cAddr, &len); //rxbufに受信データを格納
+			n = lwip_recvfrom(sock1, (char*) rxbuf, sizeof(rxbuf), (int) NULL, (struct sockaddr*) &cAddr, &len); //rxbufに受信データを格納
 			if (n > 0) {
-				timeout = 0;
 				if (n < sizeof(struct receive_data)) {
 					printf("invalid data : \r\n"); //データが欠損しているのでループを送る
 					continue;
@@ -106,6 +91,22 @@ void UDPSendReceive(void const *argument) {
 				struct receive_data *d = (struct receive_data*) &rxbuf; //rxbufの位置にreceive_data構造体を作る。(疑似的にデータが変換される)
 				memcpy(&ros_data, d, sizeof(struct receive_data)); //受信データをコピーする
 				printf("omni1:%f\r\nomni2:%f\r\nomni3:%f\r\nomni4:%f\r\n", ros_data.omni1_power, ros_data.omni2_power, ros_data.omni3_power, ros_data.omni4_power);
+
+				//callback
+				struct send_data* sd = (struct send_data*)&txbuf; //txbufの位置に重ねてsdを宣言
+				memcpy(sd, &f7_data, sizeof(struct send_data)); //送信データをコピーする
+				iBytesWritten = lwip_sendto(sock1, (char*)txbuf, sizeof(txbuf), 0, (struct sockaddr*)&cAddr, sizeof(cAddr));
+				//iBytesWritten = lwip_sendto(sock, (char*)"hello world from f7!", 256, 0, (struct sockaddr*)&cAddr, sizeof(cAddr));
+				if (iBytesWritten > 0) {
+					printf("success send data\r\n");
+					if (iBytesWritten < sizeof(struct send_data)) {
+						printf("send data invalid\r\n");
+					}else{
+						printf("send data is perfect\r\n");
+					}
+				}else{
+					printf("failed send data\r\n");
+				}
 			}
 		}
 		osDelay(10);
